ad04ec4c3c33e60ce3953b25b956cfcbc49f0a11
sumitbhujel <arginbhujel1834@gmail.com>
2025-12-13 19:34:38 +0545

    update file

--- CounterApp_REACT/src/App.jsx
+++ CounterApp_REACT/src/App.jsx
@@ -1,37 +1,69 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+const initialState = {
+  count: 0,
+  declick: 0,
+  inclick: 0,
+};
+const CounterREducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+      return {
+        ...state,
+        count: state.count + 1,
+        inclick: state.inclick + 1,
+      };
+    case "DEC":
+      return {
+        ...state,
+        count: state.count - 1,
+        declick: state.declick + 1,
+      };
+    case "RESET":
+      return {
+        ...state,
+        count: 0,
+        inclick: 0,
+        declick: 0,
+      };
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [inclick, setinClick] = useState(0);
-  const [declick, setdeClick] = useState(0);
+  const [{ count, inclick, declick }, dispatch] = useReducer(
+    CounterREducer,
+    initialState
+  );
 
-  const reset=()=>{
-    setCount(0);
-    setdeClick(0);
-    setinClick(0);
-  }
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
 
   const increament = () => {
-    setCount(count + 1);
-    setinClick(inclick + 1);
+    dispatch({ type: "INC" });
   };
   const decreament = () => {
-    setCount(count - 1);
-    setdeClick(declick + 1);
+    dispatch({ type: "DEC" });
   };
   return (
     <>
-    <h1 className="h1">Simple Counter App Using React</h1>
-    <div className="parent">
-     
+      <h1 className="h1">Simple Counter App Using React</h1>
+      <div className="parent">
         <h1>{count}</h1>
-      <button onClick={increament}>Increament</button>
-      <button className="de" onClick={decreament}>Decreament</button>
-      <button className="re" onClick={reset}>Reset</button>
+        <button onClick={increament}>Increament</button>
+        <button className="de" onClick={decreament}>
+          Decreament
+        </button>
+        <button className="re" onClick={reset}>
+          Reset
+        </button>
 
-      <p>Clicks on increament : {inclick} </p>
-      <p>Clicks on Decreament : {declick} </p>
-      </div></>
+        <p>Clicks on increament : {inclick} </p>
+        <p>Clicks on Decreament : {declick} </p>
+      </div>
+    </>
   );
 }
 
